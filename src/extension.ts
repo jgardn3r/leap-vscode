32342805fe70a4a48c5c0c787326b14f8ced9471
Joshua Gardner <joshgardner2000@gmail.com>
2023-08-20 16:20:56 +1000

    Convert extension.ts indentation to spaces

--- src/extension.ts
+++ src/extension.ts
@@ -2,14 +2,14 @@ import * as vscode from 'vscode';
 import { SearchOption, Widget } from './widget';
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand('leap-vscode.forwardSearch', () => {
-		let widget = new Widget(context, new Set([SearchOption.forward]));
-		widget.show();
-	}));
-	context.subscriptions.push(vscode.commands.registerCommand('leap-vscode.backwardSearch', () => {
-		let widget = new Widget(context, new Set([SearchOption.backward]));
-		widget.show();
-	}));
+    context.subscriptions.push(vscode.commands.registerCommand('leap-vscode.forwardSearch', () => {
+        let widget = new Widget(context, new Set([SearchOption.forward]));
+        widget.show();
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('leap-vscode.backwardSearch', () => {
+        let widget = new Widget(context, new Set([SearchOption.backward]));
+        widget.show();
+    }));
 }
 
 export function deactivate() { }
